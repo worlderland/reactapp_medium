06cb652718fc385ea8f3a31c35a7ad39ba6433af
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2021-07-15 13:13:02 +0800

    Part 2 completed.

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -4,13 +4,20 @@ import './HelloWorld.css';
 class HelloWorld extends Component {
     constructor(props) { //constructor() is a special function to perform some actions when the class is used (usually referred to as “instantiation”)
         super(props); //call out to its parent constructor
-        this.state = { greeting: 'Ciao' }; //sets a property on the class called “state”
+        this.state = { greeting: 'Hello' }; //sets a property on the class called “state”
+        this.frenchify = this.frenchify.bind(this); //This tells Javascript “Hey, any time you see a reference to 'this' inside of the frenchify function, I want you specifically to refer to ME.”
+    }
+
+    frenchify () {
+        this.setState({greeting: 'Bonjour'})
     }
 
     render() {
         return (
             <div className="HelloWorld">
-                {this.state.greeting} {this.props.name} {this.props.surname}
+                {this.state.greeting} {this.props.name} {this.props.surname}!
+                <br/>
+                <button onClick={this.frenchify}>Frenchify!</button>
             </div>
         )
     }
