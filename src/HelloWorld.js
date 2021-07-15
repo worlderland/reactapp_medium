5a9633af2addada7cac7f3ce5a3b76a75b4261a2
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2021-07-15 12:00:17 +0800

    Part 2, introducing state

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import './HelloWorld.css';
 
 class HelloWorld extends Component {
+    constructor(props) { //constructor() is a special function to perform some actions when the class is used (usually referred to as “instantiation”)
+        super(props); //call out to its parent constructor
+        this.state = { greeting: 'Ciao' }; //sets a property on the class called “state”
+    }
+
     render() {
         return (
             <div className="HelloWorld">
-                Hello {this.props.name} {this.props.surname}
+                {this.state.greeting} {this.props.name} {this.props.surname}
             </div>
         )
     }
