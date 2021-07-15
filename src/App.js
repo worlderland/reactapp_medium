b7e7e4af160ea4d137d194ac2ef41c854a416af9
Alessandro Coraglia <info.alessandrocoraglia@gmail.com>
2021-07-15 11:30:58 +0800

    Part 2, end of "separating out a component"

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import HelloWorld from './HelloWorld'
 
 const App = () => {
   return (
-    <div className="App">
-      Hello World
+    <div className = "App">
+      <HelloWorld />
+      <HelloWorld />
     </div>
   )
 }
---
+++ src/HelloWorld.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HelloWorld.css';
+
+const HelloWorld = () => {
+    return (
+        <div className="HelloWorld">
+            Hello World
+        </div>
+    )
+}
+
+export default HelloWorld
